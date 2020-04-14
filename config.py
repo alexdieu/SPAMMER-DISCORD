993d3ad5161b12652de0e825e391055156fbc04a
alexdieu <37957299+alexdieu@users.noreply.github.com>
2020-04-14 12:51:05 +0200

    Update config.py
    Erreur : TOKEN N'EXISTE PAS CORRIGE

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 #Configuration basique (Pour tous les bots)
 TokenDeLutilisateur = [''] # Token de l'utilisateur ici , les comptes mutiplent marchent. 
-DiscordSalon = '' # Channel ID,
+DiscordSalon = '' # Channel ID
 
 #Images spammer
 Images = 'C:\\Users\\Public\\Images\\City skyline\\Photos de jeux\\' # Location des images ! TOUJOURS DOUBLE SLASH ET SE TERMINER AVEC CE DOUBLE SLASH !VOUS DEVEZ LE MODIFIER POUR L'UTILISER !
