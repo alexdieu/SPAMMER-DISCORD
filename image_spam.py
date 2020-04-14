2d82ffe72031a45ab936598857d2499926df3b92
alexdieu <37957299+alexdieu@users.noreply.github.com>
2020-04-14 12:50:08 +0200

    Update image_spam.py

--- image_spam.py
+++ image_spam.py
@@ -14,7 +14,7 @@ async def on_ready():
     while not client.is_closed:
             UpImage = random.choice(os.listdir(Images)) 
             print(UpImage)
-            await client.send_file(discord.Object(id=DiscordSalon), Image + UpImage)
+            await client.send_file(discord.Object(id=DiscordSalon), Images + UpImage)
             await asyncio.sleep(0.7) # Change la vitesse à laquelle les images sont affichées
 
 client.run(token, bot=False)
